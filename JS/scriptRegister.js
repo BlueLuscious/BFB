676b86fe7101798cf942824c212d068b60aa4d31
BlueLuscious <lucio.200152@gmail.com>
2023-06-29 23:44:30 -0300

    Create scriptRegister.js

---
+++ JS/scriptRegister.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    // validar formulario
+    logInButton.addEventListener('click', function (event) {
+        event.preventDefault()
+        let password = passwordInput.value
+        if (password.length < 6 || password.length > 12) {
+            alert('La contraseña debe tener entre 6 y 12 caracteres.');
+            return;
+        }
+        if (!/[A-Z]/.test(password)) {
+            alert('La contraseña debe contener al menos una letra mayúscula.');
+            return;
+        }
+        if (!/\d/.test(password)) {
+            alert('La contraseña debe contener al menos un dígito.');
+            return;
+        }
+        if (!/[^a-zA-Z0-9]/.test(password)) {
+            alert('La contraseña debe contener al menos un carácter especial.');
+            return;
+        }
+
+        // agregar otras validaciones
+
+        document.getElementById('loginForm').submit();
+    })
+
+})
