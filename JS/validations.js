09d89dbd1a9304d8655c590f8fd74b5cda499869
BlueLuscious <lucio.200152@gmail.com>
2023-07-05 01:36:26 -0300

    Update validations.js

--- JS/validations.js
+++ JS/validations.js
@@ -1,7 +1,15 @@
+const email = document.getElementById('emailInput')
+const firstName = document.getElementById('firstNameInput')
+const lastName = document.getElementById('lastNameInput')
+const username = document.getElementById('usernameInput')
+const password = document.getElementById('passwordInput')
+const repeatPassword = document.getElementById('repeatPasswordInput')
+
 const firstPartForm = document.getElementById('firstPartForm')
 const secondPartForm = document.getElementById('secondPartForm')
 const backStepButtton = document.getElementById('backStep')
 const nextStepButton = document.getElementById('nextStep')
+const signInButton = document.getElementById('signIn')
 
 const warningEmailRegister = document.getElementById('warningEmailRegister')
 const warningFirstNameRegister = document.getElementById('warningFirstNameRegister')
@@ -15,65 +23,65 @@ export class Validations {
     validateEmail(emailInput) {
         if (emailInput.value == '') {
             warningEmailRegister.innerHTML = 'The email is empty'
-            emailInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            email.style.borderBottomColor = 'rgb(195, 20, 20)'
         }
     } // validate email function
 
     validateFirstName(firstNameInput) {
         if (firstNameInput.value == '') {
             warningFirstNameRegister.innerHTML = 'The first name empty'
-            firstNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            firstName.style.borderBottomColor = 'rgb(195, 20, 20)'
         } else if (!/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+$/.test(firstNameInput.value)) {
             warningFirstNameRegister.innerHTML = 'Invalid value, only letters'
-            firstNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            firstName.style.borderBottomColor = 'rgb(195, 20, 20)'
         }
     } // validate firstname function
 
     validateLastName(lastNameInput) {
         if (lastNameInput.value == '') {
             warningLastNameRegister.innerHTML = 'The last name empty'
-            lastNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            lastName.style.borderBottomColor = 'rgb(195, 20, 20)'
             return
         } else if (!/^([a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+\s)*[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+$/.test(lastNameInput.value)) {
             warningLastNameRegister.innerHTML = 'Invalid value, only letters'
-            lastNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            lastName.style.borderBottomColor = 'rgb(195, 20, 20)'
             return
         } else {
             firstPartForm.style.display = 'none'
             secondPartForm.style.display = 'block'
-            this.style.display = 'none'
-            signIn.style.display = 'block'
-            backStep.disabled = false
-            backStep.style.cursor = 'pointer'
+            nextStepButton.style.display = 'none'
+            signInButton.style.display = 'block'
+            backStepButtton.disabled = false
+            backStepButtton.style.cursor = 'pointer'
         }
     } // // validate lastname function & display form's second part
 
     validateUsername(usernameInput) {
         if (usernameInput.value == '') {
             warningUsernameRegister.innerHTML = 'The password is empty'
-            usernameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            username.style.borderBottomColor = 'rgb(195, 20, 20)'
         }
     } // validate username function
 
     validatePassword(passwordInput) {
         if (passwordInput.value == '') {
             warningPasswordRegister.innerHTML = 'The password is empty'
-            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            password.style.borderBottomColor = 'rgb(195, 20, 20)'
         } else if (passwordInput.value.length < 6 || passwordInput.value.length > 12) {
             warningPasswordRegister.innerHTML = 'The Password must have between 6 to 12 characters'
-            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            password.style.borderBottomColor = 'rgb(195, 20, 20)'
             return
         } else if (!/[A-Z]/.test(passwordInput.value)) {
             warningPasswordRegister.innerHTML = 'The password must have at least one capital letter'
-            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            password.style.borderBottomColor = 'rgb(195, 20, 20)'
             return
         } else if (!/\d/.test(passwordInput.value)) {
             warningPasswordRegister.innerHTML = 'The password must have at least one number'
-            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            password.style.borderBottomColor = 'rgb(195, 20, 20)'
             return
         } else if (!/[^a-zA-Z0-9]/.test(passwordInput.value)) {
             warningPasswordRegister.innerHTML = 'The password must have at least one special character'
-            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            password.style.borderBottomColor = 'rgb(195, 20, 20)'
             return
         }
     } // validate password function
@@ -81,15 +89,12 @@ export class Validations {
     validateRepeatPassword(repeatPasswordInput) {
         if (repeatPasswordInput.value == '') {
             warningRepeatPasswordRegister.innerHTML = 'The repeated password is empty'
-            repeatPasswordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            repeatPassword.style.borderBottomColor = 'rgb(195, 20, 20)'
             return
         } else if (repeatPasswordInput.value != passwordInput.value) {
             warningRepeatPasswordRegister.innerHTML = 'The passwords do not match'
-            repeatPasswordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            repeatPassword.style.borderBottomColor = 'rgb(195, 20, 20)'
             return
-        } else if (repeatPasswordInput.value == passwordInput.value) {
-            passwordInput.style.borderInlineColor = 'rgb(195, 20, 20)'
-        
         }
     } // validate repeated password function
 
