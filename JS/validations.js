d6d0611b602133ad8c0047765cc479756a66c614
BlueLuscious <lucio.200152@gmail.com>
2023-07-05 01:23:30 -0300

    Update validations.js

--- JS/validations.js
+++ JS/validations.js
@@ -1,4 +1,59 @@
+const firstPartForm = document.getElementById('firstPartForm')
+const secondPartForm = document.getElementById('secondPartForm')
+const backStepButtton = document.getElementById('backStep')
+const nextStepButton = document.getElementById('nextStep')
+
+const warningEmailRegister = document.getElementById('warningEmailRegister')
+const warningFirstNameRegister = document.getElementById('warningFirstNameRegister')
+const warningLastNameRegister = document.getElementById('warningLastNameRegister')
+const warningUsernameRegister = document.getElementById('warningUsernameRegister')
+const warningPasswordRegister = document.getElementById('warningPasswordRegister')
+const warningRepeatPasswordRegister = document.getElementById('warningRepeatPasswordRegister')
+
 export class Validations {
+
+    validateEmail(emailInput) {
+        if (emailInput.value == '') {
+            warningEmailRegister.innerHTML = 'The email is empty'
+            emailInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+        }
+    } // validate email function
+
+    validateFirstName(firstNameInput) {
+        if (firstNameInput.value == '') {
+            warningFirstNameRegister.innerHTML = 'The first name empty'
+            firstNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+        } else if (!/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+$/.test(firstNameInput.value)) {
+            warningFirstNameRegister.innerHTML = 'Invalid value, only letters'
+            firstNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+        }
+    } // validate firstname function
+
+    validateLastName(lastNameInput) {
+        if (lastNameInput.value == '') {
+            warningLastNameRegister.innerHTML = 'The last name empty'
+            lastNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        } else if (!/^([a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+\s)*[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+$/.test(lastNameInput.value)) {
+            warningLastNameRegister.innerHTML = 'Invalid value, only letters'
+            lastNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        } else {
+            firstPartForm.style.display = 'none'
+            secondPartForm.style.display = 'block'
+            this.style.display = 'none'
+            signIn.style.display = 'block'
+            backStep.disabled = false
+            backStep.style.cursor = 'pointer'
+        }
+    } // // validate lastname function & display form's second part
+
+    validateUsername(usernameInput) {
+        if (usernameInput.value == '') {
+            warningUsernameRegister.innerHTML = 'The password is empty'
+            usernameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+        }
+    } // validate username function
 
     validatePassword(passwordInput) {
         if (passwordInput.value == '') {
@@ -21,6 +76,6 @@ export class Validations {
             passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
             return
         }
-    }
+    } // validate password function
 
 }
