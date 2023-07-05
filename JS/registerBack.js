91eb8e3515ac5c5cabf0fc2b2d6075efac70736c
BlueLuscious <lucio.200152@gmail.com>
2023-07-05 01:55:01 -0300

    Update registerBack.js

--- JS/registerBack.js
+++ JS/registerBack.js
@@ -1,35 +1,39 @@
 import { Validations } from './validations'
+
+const emailInput = document.getElementById('emailInput')
+const firstNameInput = document.getElementById('firstNameInput')
+const lastNameInput = document.getElementById('lastNameInput')
+const usernameInput = document.getElementById('usernameInput')
+const passwordInput = document.getElementById('passwordInput')
+const repeatPasswordInput = document.getElementById('repeatPasswordInput')
+
+const nextStepButton = document.getElementById('nextStep')
+const signInButton = document.getElementById('signIn')
+const signUpForm = document.getElementById('signUpForm')
+
+const warningEmailRegister = document.getElementById('warningEmailRegister')
+const warningFirstNameRegister = document.getElementById('warningFirstNameRegister')
+const warningLastNameRegister = document.getElementById('warningLastNameRegister')
+const warningUsernameRegister = document.getElementById('warningUsernameRegister')
+const warningPasswordRegister = document.getElementById('warningPasswordRegister')
+const warningRepeatPasswordRegister = document.getElementById('warningRepeatPasswordRegister')
+
 const validator = new Validations()
 
-window.addEventListener('DOMContentLoaded', function () {
-
-    const emailInput = document.getElementById('emailInput')
-    const firstNameInput = document.getElementById('firstNameInput')
-    const lastNameInput = document.getElementById('lastNameInput')
-    const usernameInput = document.getElementById('usernameInput')
-    const passwordInput = document.getElementById('passwordInput')
-    const repeatPasswordInput = document.getElementById('repeatPasswordInput')
-
-    const nextStepButton = document.getElementById('nextStep')
-    const signInButton = document.getElementById('signIn')
-    const signUpForm = document.getElementById('signUpForm')
-
-
-    nextStepButton.addEventListener('click', function () {
-        validator.validateEmail(emailInput.value)
-        validator.validateFirstName(firstNameInput.value)
-        validator.validateLastName(lastNameInput.value)
-    })
-
-    signInButton.addEventListener('click', function () {
-        validator.validateUsername(usernameInput.value)
-        validator.validatePassword(passwordInput.value)
-        validator.validateRepeatPassword(repeatPasswordInput.value)
-        
-        signUpForm.submit()
-    })
-
+nextStepButton.addEventListener('click', function () {
+    validator.validateEmail(emailInput, warningEmailRegister)
+    validator.validateFirstName(firstNameInput, warningFirstNameRegister)
+    validator.validateLastName(lastNameInput, warningLastNameRegister)
 })
+
+signInButton.addEventListener('click', function () {
+    validator.validateUsername(usernameInput, warningUsernameRegister)
+    validator.validatePassword(passwordInput, warningPasswordRegister)
+    validator.validateRepeatPassword(repeatPasswordInput, warningRepeatPasswordRegister)
+
+    signUpForm.submit()
+})
+
 
 
 
