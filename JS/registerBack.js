d10c82240af50c55eef75590b6d794d16c350cfc
BlueLuscious <lucio.200152@gmail.com>
2023-07-05 02:04:11 -0300

    Update registerBack.js

--- JS/registerBack.js
+++ JS/registerBack.js
@@ -1,35 +1,41 @@
 import { Validations } from './validations'
 
-const emailInput = document.getElementById('emailInput')
-const firstNameInput = document.getElementById('firstNameInput')
-const lastNameInput = document.getElementById('lastNameInput')
-const usernameInput = document.getElementById('usernameInput')
-const passwordInput = document.getElementById('passwordInput')
-const repeatPasswordInput = document.getElementById('repeatPasswordInput')
+window.addEventListener('DOMContentLoaded', function () {
 
-const nextStepButton = document.getElementById('nextStep')
-const signInButton = document.getElementById('signIn')
-const signUpForm = document.getElementById('signUpForm')
+    const emailInput = document.getElementById('emailInput')
+    const firstNameInput = document.getElementById('firstNameInput')
+    const lastNameInput = document.getElementById('lastNameInput')
+    const usernameInput = document.getElementById('usernameInput')
+    const passwordInput = document.getElementById('passwordInput')
+    const repeatPasswordInput = document.getElementById('repeatPasswordInput')
 
-const warningEmailRegister = document.getElementById('warningEmailRegister')
-const warningFirstNameRegister = document.getElementById('warningFirstNameRegister')
-const warningLastNameRegister = document.getElementById('warningLastNameRegister')
-const warningUsernameRegister = document.getElementById('warningUsernameRegister')
-const warningPasswordRegister = document.getElementById('warningPasswordRegister')
-const warningRepeatPasswordRegister = document.getElementById('warningRepeatPasswordRegister')
+    const nextStepButton = document.getElementById('nextStep')
+    const signInButton = document.getElementById('signIn')
+    const signUpForm = document.getElementById('signUpForm')
 
-const validator = new Validations()
+    const warningEmailRegister = document.getElementById('warningEmailRegister')
+    const warningFirstNameRegister = document.getElementById('warningFirstNameRegister')
+    const warningLastNameRegister = document.getElementById('warningLastNameRegister')
+    const warningUsernameRegister = document.getElementById('warningUsernameRegister')
+    const warningPasswordRegister = document.getElementById('warningPasswordRegister')
+    const warningRepeatPasswordRegister = document.getElementById('warningRepeatPasswordRegister')
 
-nextStepButton.addEventListener('click', function () {
-    validator.validateEmail(emailInput, warningEmailRegister)
-    validator.validateFirstName(firstNameInput, warningFirstNameRegister)
-    validator.validateLastName(lastNameInput, warningLastNameRegister)
-})
+    const validator = new Validations()
 
-signInButton.addEventListener('click', function () {
-    validator.validateUsername(usernameInput, warningUsernameRegister)
-    validator.validatePassword(passwordInput, warningPasswordRegister)
-    validator.validateRepeatPassword(repeatPasswordInput, warningRepeatPasswordRegister)
+    nextStepButton.addEventListener('click', function () {
+        validator.validateEmail(emailInput.value, warningEmailRegister)
+        validator.validateFirstName(firstNameInput.value, warningFirstNameRegister)
+        validator.validateLastName(lastNameInput.value, warningLastNameRegister)
+    })
 
-    signUpForm.submit()
+    signInButton.addEventListener('click', function (event) {
+        event.preventDefault()
+
+        validator.validateUsername(usernameInput.value, warningUsernameRegister)
+        validator.validatePassword(passwordInput.value, warningPasswordRegister)
+        validator.validateRepeatPassword(repeatPasswordInput.value, warningRepeatPasswordRegister)
+
+        signUpForm.submit()
+    })
+
 })
