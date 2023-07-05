d9ea717a4bf339a85057e256c4ea38f5c19ecf4a
BlueLuscious <lucio.200152@gmail.com>
2023-07-05 02:33:33 -0300

    Create validations.js

---
+++ JS/module/validations.js
@@ -0,0 +1,79 @@
+export class Validations {
+    validateEmail(emailInput) {
+        if (emailInput.value == '') {
+            warningEmailRegister.innerHTML = 'The email is empty'
+            emailInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+        }
+    } // validate email function
+
+    validateFirstName(firstNameInput) {
+        if (firstNameInput.value == '') {
+            warningFirstNameRegister.innerHTML = 'The first name empty'
+            firstNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+        } else if (!/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+$/.test(firstNameInput.value)) {
+            warningFirstNameRegister.innerHTML = 'Invalid value, only letters'
+            firstNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+        }
+    } // validate firstname function
+
+    validateLastName(lastNameInput) {
+        if (lastNameInput.value == '') {
+            warningLastNameRegister.innerHTML = 'The last name empty'
+            lastNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        } else if (!/^([a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+\s)*[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+$/.test(lastNameInput.value)) {
+            warningLastNameRegister.innerHTML = 'Invalid value, only letters'
+            lastNameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        } else {
+            firstPartForm.style.display = 'none'
+            secondPartForm.style.display = 'block'
+            nextStepButton.style.display = 'none'
+            signInButton.style.display = 'block'
+            backStepButtton.disabled = false
+            backStepButtton.style.cursor = 'pointer'
+        }
+    } // // validate lastname function & display form's second part
+
+    validateUsername(usernameInput) {
+        if (usernameInput.value == '') {
+            warningUsernameRegister.innerHTML = 'The password is empty'
+            usernameInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+        }
+    } // validate username function
+
+    validatePassword(passwordInput) {
+        if (passwordInput.value == '') {
+            warningPasswordRegister.innerHTML = 'The password is empty'
+            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+        } else if (passwordInput.value.length < 6 || passwordInput.value.length > 12) {
+            warningPasswordRegister.innerHTML = 'The Password must have between 6 to 12 characters'
+            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        } else if (!/[A-Z]/.test(passwordInput.value)) {
+            warningPasswordRegister.innerHTML = 'The password must have at least one capital letter'
+            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        } else if (!/\d/.test(passwordInput.value)) {
+            warningPasswordRegister.innerHTML = 'The password must have at least one number'
+            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        } else if (!/[^a-zA-Z0-9]/.test(passwordInput.value)) {
+            warningPasswordRegister.innerHTML = 'The password must have at least one special character'
+            passwordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        }
+    } // validate password function
+
+    validateRepeatPassword(repeatPasswordInput, passwordInput) {
+        if (repeatPasswordInput.value == '') {
+            warningRepeatPasswordRegister.innerHTML = 'The repeated password is empty'
+            repeatPasswordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        } else if (repeatPasswordInput.value != passwordInput.value) {
+            warningRepeatPasswordRegister.innerHTML = 'The passwords do not match'
+            repeatPasswordInput.style.borderBottomColor = 'rgb(195, 20, 20)'
+            return
+        }
+    } // validate repeated password function
+}
